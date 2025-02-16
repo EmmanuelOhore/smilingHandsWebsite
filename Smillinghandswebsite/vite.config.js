196a02a88bed60cbdef14adfb4d8d5d3580336c5
Ohore Efemena Emmanuel <emmanuelohore2003@gmail.com>
2025-02-16 09:31:25 +0100

    dd

--- Smillinghandswebsite/vite.config.js
+++ Smillinghandswebsite/vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure Netlify looks for the correct build directory
+  },
 });
